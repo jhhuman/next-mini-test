4080fd0bff4efc4f4c4e1f7540d644ec83b83a91
Hendrik Human <61615033+jhhuman@users.noreply.github.com>
2020-11-14 16:30:24 +0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+function Home() {
+  return (
+    <>
+      <h1>My Homepage</h1>
+      <Image
+        src="/iphone-1.jpg"
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
+      <p>Welcome to my homepage!</p>
+    </>
+  )
+}
+
+export default Home
